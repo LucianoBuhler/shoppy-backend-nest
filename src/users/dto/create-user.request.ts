2a8047a0492b388dbf2c02ab4cf0b603b231fa52
LucianoBuhler <lucianobuhler@gmail.com>
2025-04-03 22:20:32 -0300

    Added users module with controller and service; added validation using class-validator with dto; added the config using @nestjs/config; Logging improvements using pino

---
+++ src/users/dto/create-user.request.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsStrongPassword } from "class-validator";
+// import "class-validator/types/decorator/string/IsEmail"; // Ensure proper type inference
+// import "class-validator/types/decorator/string/IsStrongPassword"; // Ensure proper type inference
+
+export class CreateUserRequest {
+  @IsEmail()
+  email: string;
+
+  @IsStrongPassword()
+  password: string;
+}
