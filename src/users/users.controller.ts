f4f1a243c00272f72c78ddd4174539616a385300
LucianoBuhler <lucianobuhler@gmail.com>
2025-04-04 15:27:32 -0300

    Added interceptor for user controller to handle the form type properly

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseInterceptors } from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.request';
 import { UsersService } from './users.service';
+import { NoFilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @UseInterceptors(NoFilesInterceptor())
   createUser(@Body() request: CreateUserRequest) {
     return this.usersService.createUser(request);
   }
